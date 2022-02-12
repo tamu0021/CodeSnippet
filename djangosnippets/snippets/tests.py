a3bdf8c103a835147ad3d9cdafd4cc78113d54cd
Tamu0021 <czr29479@gmail.com>
2022-02-12 15:39:01 +0900

    HelloWorldテスト。 テストコードやトップページへのルーティング設定完了

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,3 +1,14 @@
+from urllib import request
 from django.test import TestCase
+from django.http import HttpRequest
+from snippets.views import top
 
-# Create your tests here.
+class TopPageViewTest(TestCase):
+    def test_top_returns_200(self):
+        # HttpRequestのオブジェクト作成
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def top(request):
+    return HttpResponse(b"Hello World")
